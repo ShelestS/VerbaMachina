3f79b888a8c96fe3cd5aefa765493150d0ea8e1c
Marvin Naessig <marvinabn@gmail.com>
2023-03-06 23:24:53 -0800

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.markdown("""# This is a header
-## This is a sub header
-This is text""")
+st.markdown("""# Pesona AI
+## Ask me anything""")
+
+image = '/marvinabn/VerbaMachina/images.jpg'
+#st.image(image, caption='Harry Bot',use_column_width=True)
+st.image(image, caption= 'Harry', width=650)
+
+query = st.text_input('User:')
